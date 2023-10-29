2895d7b728fcac565275db24028c79fe61643bac
noproblemdude <leonardrolandvictor@googlemail.com>
2023-10-29 21:21:40 +0100

    Endabgabe ohne Dev dependencies
    Keine dev dependency im Prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
 
 const pages = ['index', 'Home', "ratings"];
 
@@ -60,7 +59,6 @@ module.exports = {
     ),
     [
       new CleanWebpackPlugin(),
-      new ESLintPlugin(),
     ],
   ),
 };
