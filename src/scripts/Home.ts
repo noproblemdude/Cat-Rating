69c689a49a3ee4edb2ab280881c2cb4ceffe3061
noproblemdude <leonardrolandvictor@googlemail.com>
2024-01-24 17:53:57 +0100

    json loaded as file

--- src/scripts/Home.ts
+++ src/scripts/Home.ts
@@ -1,11 +1,14 @@
 import * as Leaflet from 'leaflet'
 import {parkingSpots } from './parkingSpot';
+import data from './SCOOTERABSTELLOGD.json';
 
 const leafMap = <HTMLButtonElement>document.getElementById("map");
 const loadButton = <HTMLButtonElement>document.getElementById("loadbutton");
 
 
 loadButton.addEventListener('click',setMarkers);
+
+console.log(data.totalFeatures);
 
 function setMarkers(){
 
@@ -14,31 +17,11 @@ function setMarkers(){
   for (let index = 0; index < spots.getSpotNumber(); index++) {
       const marker = Leaflet.marker([spots.getSpot(index).getLongitude(),spots.getSpot(index).getLatitude()]).addTo(map);
       let popupString = "<b>" + spots.getSpot(index).getAddress() + "</b>"
-      popupString = popupString + "<br>Anzahl Scooter:" + spots.getSpot(index).getScooterNumber;
+      popupString = popupString + "<br>Anzahl Scooter:" + spots.getSpot(index).getScooterNumber();
       marker.bindPopup(popupString).openPopup();
   }
 
 }
-
-
-/*
-async function fetchParkingSpots(){
-
-    const url = 'https://corsproxy.io/?' + encodeURIComponent('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SCOOTERABSTELLOGD&srsName=EPSG:4326&outputFormat=json');
-
-    const url = 'https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SCOOTERABSTELLOGD&srsName=EPSG:4326&outputFormat=json';
-    const parkingsResponse = await fetch(url/*, {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      headers: {
-        'Host':'data.wien.gv.at',
-        'Origin':'213.47.84.184'
-      },
-    });
-      
-    console.log(parkingsResponse)
-}
-*/
 
 const map = Leaflet.map(leafMap).setView([48.2082, 16.3719], 13);
 
